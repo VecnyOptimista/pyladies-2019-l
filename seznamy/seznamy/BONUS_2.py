319130f59caf0bfd9193b9450920d1967c62e095
Michal Semenka <michal.semenka@nic.cz>
2019-04-24 20:41:27 +0200

    Fix solution of BONUS_2.py

--- seznamy/seznamy/BONUS_2.py
+++ seznamy/seznamy/BONUS_2.py
@@ -1,10 +1,13 @@
 # Napis funkci, ktera dostane seznam a bude vracet novy seznam, ve kterem budou pouze dva nahodne vybrane prvky z puvodniho seznamu
+import random
+
 
 seznam = [1, 5, 4, 12, 3, 7, 11]
 
 def zpracuj(seznam):
-    vysledek = seznam[:-2]
-    vysledek.sort()
+    vysledek = []
+    for step in range(2):
+        vysledek.append(seznam[random.randint(0, len(seznam) - 1)])
     return vysledek
 
 print(zpracuj(seznam))
